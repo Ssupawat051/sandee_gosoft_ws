2f2288accb0a234700716e90f74231ae39497ba5
Ssupawat051 <supawad.mark051@gmail.com>
2023-08-16 00:12:46 -0700

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sommark/urdf_ws/src
+# Build directory: /home/sommark/urdf_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
