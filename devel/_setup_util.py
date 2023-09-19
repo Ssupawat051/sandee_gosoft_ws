bf052f7cbbf816a32accd567c103d094e05e391b
supawat <supawad.mark051@gmail.com>
2023-09-18 19:43:24 -0700

    pj

--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/sommark/urdf_ws/devel;/home/sommark/new_workspace/devel;/opt/ros/noetic'.split(';')
+            CMAKE_PREFIX_PATH = r'/opt/ros/noetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
