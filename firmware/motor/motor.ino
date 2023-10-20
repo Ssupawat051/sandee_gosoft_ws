001f94ff34feff33c64d1cb960a93a1a4f6d4205
supawat <134144947+Ssupawat051@users.noreply.github.com>
2023-10-20 14:55:58 +0700

    Update motor.ino

--- firmware/motor/motor.ino
+++ firmware/motor/motor.ino
@@ -1,121 +1,94 @@
-#include <ros.h>
-#include <std_msgs/String.h>
-#include <geometry_msgs/Twist.h>
+#include <ArduinoHardware.h>
+#include <ros.h> 
+#include <geometry_msgs/Twist.h> 
+#include <std_msgs/Float32.h> 
 #include <Encoder.h>
 
-int pwm1 = 38 ;
-int dir1 = 39;
-int pwm2 = 44;
-int dir2= 45;
+Encoder myEncL(2, 3);
+Encoder myEncR(18, 19);
+long oldPositionL  = 0;
+long oldPositionR  = 0;
+ros::NodeHandle nh;
 
-double prev_time = 0;
-double robot_x = 0;
-double robot_y = 0;
-double robot_theta = 0;
 
-Encoder myEncoder1(2, 3); //encoderR Pin:a,b
-Encoder myEncoder2(18, 19);//encoderL Pin:a,b
+geometry_msgs::Twist msg;
+std_msgs::Float32 encL_msg;
+std_msgs::Float32 encR_msg;
 
-const float WheelRad = 0.12192; // in to m
-const int PulseRv = 3246; // PULSES_PER_REVOLUTION
-const float WheelSeparation = 0.422;
+ros::Publisher EncL("Enc_L", &encL_msg);
+ros::Publisher EncR("Enc_R", &encR_msg);
 
-ros::NodeHandle  nh;
+  long newPositionL;
+  long newPositionR;
+  
+void roverCallBack(const geometry_msgs::Twist& cmd_vel)
+{
 
-geometry_msgs::Twist Enc_msg;
-ros::Publisher Pub ("Enc_mt", &Enc_msg);
+  double x = cmd_vel.linear.x;
+        double z = cmd_vel.angular.z;
 
-unsigned long lastUpdateTime = 0; // last update encoder
-const unsigned long updateInterval = 100; //encoder update time 0.1s
+  double moveL = x+(z/2);
+  double moveR = x-(z/2);
 
-void setcallback(const geometry_msgs::Twist& cmd_vel){
-  double vel_x = cmd_vel.linear.x;
-  double vel_th = cmd_vel.angular.z;
-  double moveR = vel_x+(vel_th/2);
-  double moveL = vel_x-(vel_th/2);
-
-  //motor R
-  if(moveR>0.0){
-    analogWrite(38,64);
-    digitalWrite(39,1);
-    }
-  else if(moveR<0.0){
-    analogWrite(38,64);
-    digitalWrite(39,0);
-    }
-  else{
-    analogWrite(38,0);
-    digitalWrite(39,0);
-    }
-    
-  //motor L
-  if(moveL>0.0){
-    analogWrite(44,64);
-    digitalWrite(45,0);
-    }
-  else if(moveL<0.0){
-    analogWrite(44,64);
-    digitalWrite(45,1);
-    }
-  else{
-    analogWrite(44,0);
-    digitalWrite(45,0);
+  
+if (moveL>0.0){
+        analogWrite(6,max(min(moveL*100,128),65));
+        digitalWrite(7,0);
+    }else if (moveL<0.0){
+        analogWrite(6,max(min(abs(moveL)*100,128),65));
+        digitalWrite(7,1);
+    }else{ 
+        analogWrite(6,0);
+        digitalWrite(7,0);
   }
+
+if (moveR>0.0){
+        analogWrite(44,max(min(moveR*100,128),65));
+        digitalWrite(45,0);
+    }else if (moveR<0.0){
+        analogWrite(44,max(min(abs(moveR)*100,128),65));
+        digitalWrite(45,1);
+    }else{ 
+        analogWrite(44,0);
+        digitalWrite(45,0);
+        }
+
+ 
 }
-ros::Subscriber <geometry_msgs::Twist> Sub("/cmd_vel", setcallback);
+ros::Subscriber <geometry_msgs::Twist> Motor("/cmd_vel", roverCallBack);
 
-void robotpose(double motor1_speed, double motor2_speed) {
-  
-  double avg_speed = (motor1_speed + motor2_speed) / 2.0;
-
-  double distance = avg_speed * 0.1;
-
-  double delta_theta = (motor2_speed - motor1_speed) / WheelSeparation * 0.1;
-
-  robot_x += distance * cos(robot_theta + delta_theta / 2);
-  robot_y += distance * sin(robot_theta + delta_theta / 2);
-  robot_theta += delta_theta;
- /* Serial.print("Robot Position: x = ");
-  Serial.print(robot_x);
-  Serial.print(", y = ");
-  Serial.print(robot_y);
-  Serial.print(", theta = ");
-  Serial.println(robot_theta);*/
-}
-  
-void setup() {
-  nh.initNode();
-  nh.advertise(Pub);
-  nh.subscribe(Sub);
-  
-  Serial.begin(115200);
-  
-  pinMode(38,OUTPUT);
-  pinMode(39,OUTPUT);
+void setup()
+{
+  pinMode(6,OUTPUT);  
+  pinMode(7,OUTPUT); 
   pinMode(44,OUTPUT);
-  pinMode(45,OUTPUT);
-}
+  pinMode(45,OUTPUT);   
 
-void loop() {
-  nh.spinOnce();
+  nh.initNode();
+  nh.subscribe(Motor);
+  nh.advertise(EncL);
+  nh.advertise(EncR);
+} 
+
+void loop()
+{
+    newPositionL = myEncL.read();
+  newPositionR = myEncR.read()*-1;
   
-  unsigned long currentTime = millis();
-  if (currentTime - lastUpdateTime >= updateInterval) {
-    lastUpdateTime = currentTime;
-    
-    long count1 = myEncoder1.read();
-    long count2 = myEncoder2.read();
-
-    //speed motor : m/s
-    double motor1_speed = (double)count1 / PulseRv * (2 * 3.14 * WheelRad);
-    double motor2_speed = (double)count2 / PulseRv * (2 * 3.14 * WheelRad);
-   
-    //print(currentTime);
-    
-    //robotpose(motor1_speed , motor2_speed);
-    Enc_msg.linear.x = motor1_speed;
-    Enc_msg.linear.y = motor2_speed;
-    Pub.publish( &Enc_msg );
+  if (newPositionL != oldPositionL) {
+    oldPositionL = newPositionL;
+     encL_msg.data = newPositionL;
   }
-  
+  if (newPositionR != oldPositionR) {
+    oldPositionR = newPositionR;
+     encR_msg.data = newPositionR;
+  }
+  if((newPositionL>1000000)||(newPositionR>1000000)||(newPositionL<-1000000)||(newPositionR<-1000000)){
+  myEncL.write(0);
+  myEncR.write(0);
+  }
+  EncL.publish( &encL_msg );
+  EncR.publish( &encR_msg );
+  nh.spinOnce();
+  delay(10);
 }
